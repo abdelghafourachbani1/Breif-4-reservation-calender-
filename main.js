0b6b3158c80595907d1ffa872add3b068c9c1032
abdelghafour <achbaniabdelghafour8@gmail.com>
2025-10-31 22:09:53 +0100

    using dom i bring the elemnet that i need from html

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 /////////////////// bring the element from html //////////////////
 
+let activeday = document.querySelectorAll(".day");
+let tform = document.getElementById("resesrvationform");
+let closeform = document.getElementById("closeform");
+let ajoutebtn = document.getElementById("btn-ajoute");
+let saveform = document.getElementById("saveform");
+let pageblur = document.querySelector(".blur-inactive");
+let reservation = document.getElementsByClassName("reservation");
